d86964f23f63d6795ffff69369bc8432c3875320
BenWoolford30 <benwoolford30@gmail.com>
2022-04-23 13:11:13 +0100

    C++ Hello World project
    Hello World in C++

---
+++ cpp_hello_world.cpp
@@ -0,0 +1,27 @@
+#pragma region ""
+
+#include <iostream>
+#include <string>;
+using namespace std;
+
+#pragma endregion
+/*
+        Welcome to your first programme- to start run the program by pressing the green play button OR by pressing F5.
+
+        The very first thing you will see is a window appears with text saying Hello World!
+
+        Once you see it close it and try changing the text to output something else.
+
+    */
+#pragma region ""
+int main()
+{
+#pragma endregion
+        
+    cout << "Hello World!";  
+
+#pragma region ""
+}
+#pragma endregion
+
+
